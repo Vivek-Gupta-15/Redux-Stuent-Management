a99021997cba393a3c3aedfe40a12dcdd3668fdc
Vivek-Gupta-15 <vivekgupta.151618@gmail.com>
2025-03-24 21:28:22 +0530

    Student Management System.

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/contact.css';
+function Contact() {
+  return (
+    <div className='heading'>
+      <h3> WELCOME TO THE CONTACT PAGE !!
+        
+      </h3>
+
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../css/Home.css'
+function Home() {
+  return (
+    <div className='Heading'>
+        
+        <h1>WELCOME TO THE HOME PAGE !!</h1>
+        <h4>Name: <span> VIVEK GUPTA</span></h4>
+        <h4></h4>
+        <h4>UID:<span>24BTCSE030</span></h4>
+    </div>
+  )
+}
+
+export default Home
